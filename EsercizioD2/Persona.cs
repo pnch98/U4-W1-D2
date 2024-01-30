6afb1d25e60fe90199a890f9bb76e8cfd2f03e17
Alf <cecerealfonso98@gmail.com>
2024-01-30 15:20:37 +0100

    dsdc

---
+++ EsercizioD2/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioD2
+{
+    internal class Persona
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public Persona() { }
+        public Persona(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+        public string GetDettagli() { 
+            return "Name: " + this.Name + "\nSurname: " + this.Surname + "\nAge: " + this.Age + "\n";
+        }
+    }
+}
---
+++ EsercizioD2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioD2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("Marco", "Giacomini", 34);
+            Console.WriteLine(persona1.GetDettagli());
+            Persona persona2 = new Persona();
+            Console.WriteLine("Inserisci il nome: ");
+            string nome = Console.ReadLine();
+            persona2.Name = nome;
+            Console.WriteLine("Inserisci il cognome: ");
+            string cognome = Console.ReadLine();
+            persona2.Surname = cognome;
+
+            Console.WriteLine("Inserisci età: ");
+            int eta ;
+            while(!int.TryParse(Console.ReadLine(), out eta)) {
+                Console.WriteLine("Ho detto età!");
+                Console.WriteLine("Inserisci età: ");
+            }
+            persona2.Age = eta;
+
+            Console.WriteLine(persona2.GetDettagli());
+
+
+
+        }
+    }
+}
